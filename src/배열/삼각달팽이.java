8fdd7fe5f5e6f826e98a7cb8efd8fb745b39b75c
kwonjiyun <ziyun1612@ewhain.net>
2023-12-28 12:42:33 +0900

    chap3 : 삼각달팽 - 주석 추가

--- src/배열/삼각달팽이.java
+++ src/배열/삼각달팽이.java
@@ -1,25 +1,25 @@
 package src.배열;
 
 public class 삼각달팽이 {
-    private static final int[] dx = {0, 1, -1};
-    private static final int[] dy = {1, 0, -1};
+    private static final int[] dx = {0, 1, -1}; // 변화량 , 특정 뱡항으로 이동할 때 해당 좌표값이 어떻게 변화하는지
+    private static final int[] dy = {1, 0, -1}; // 값이 변하지 않으므로 상수값으로 설정
 
     public int[] solution(int n) {
-        int[][] triangle = new int[n][n];
+        int[][] triangle = new int[n][n];// 삼각형을 표현할 2차원 배열
         int v = 1;
-        int x = 0;
+        int x = 0; // 현재 위치
         int y = 0;
         int d = 0;
 
         while (true) {
-            triangle[y][x] = v++;
-            int nx = x + dx[d];
+            triangle[y][x] = v++; // 숫자 채우기
+            int nx = x + dx[d]; // 이동
             int ny = y + dy[d];
-            if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
-                d = (d + 1) % 3;
+            if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) { //현재 방향으로는 진행할 수 없다.
+                d = (d + 1) % 3; //2를 넘어가면 다시 0으로
                 nx = x + dx[d];
                 ny = y + dy[d];
-                if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
+                if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {//전환된 방향으로도 진행할 수 없을 때 그만두기
                     break;
                 }
                 x = nx;
