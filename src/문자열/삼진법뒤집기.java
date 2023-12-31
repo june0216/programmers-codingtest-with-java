67af2be2335a9f163a0ce81ce13f875f3a951336
kwonjiyun <ziyun1612@ewhain.net>
2023-12-31 19:25:34 +0900

    chap4 : 3진법 뒤집기

---
+++ src/문자열/삼진법뒤집기.java
@@ -0,0 +1,10 @@
+package src.문자열;
+
+public class 삼진법뒤집기 {
+    public int solution(int n){
+        String str = Integer.toString(n, 3); //3진법으로 변환
+        String reversed = new StringBuilder(str).reverse().toString();//뒤집기
+        return Integer.valueOf(reversed, 3);// 3진법을 다시 정수로 변환
+        //참고  기본 int 가 필요하면 parseInt() , Integer wrapper 객체가 필요하면 valueOf() 를 사용
+    }
+}
