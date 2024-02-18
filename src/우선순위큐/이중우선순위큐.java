db3941c7304a423ba33938c613ab430ad027ece6
kwonjiyun <ziyun1612@ewhain.net>
2024-02-18 15:10:00 +0900

    우선순위 큐: 이중우선순위큐

---
+++ src/우선순위큐/이중우선순위큐.java
@@ -0,0 +1,38 @@
+package src.우선순위큐;
+import java.util.*;
+import java.io.*;
+class 이중우선순위큐 {
+    public int[] solution(String[] operations) {
+        PriorityQueue<Integer> min = new PriorityQueue<>();
+        PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
+        StringTokenizer st;
+
+        for(int i=0;i<operations.length;i++) {
+            st = new StringTokenizer(operations[i]);
+            char op = st.nextToken().charAt(0);
+            int num = Integer.parseInt(st.nextToken());
+
+            switch(op) {
+                case 'I':
+                    min.add(num);
+                    max.add(num);
+                    break;
+                case 'D':
+                    if(max.isEmpty()) break;
+                    if(num == 1) {
+                        int del = max.poll();
+                        min.remove(del);
+                    }
+                    if(num == -1) {
+                        int del = min.poll();
+                        max.remove(del);
+                    }
+            }
+        }
+
+        if(max.isEmpty())
+            return new int[] {0, 0};
+
+        return new int[] {max.peek(), min.peek()};
+    }
+}
