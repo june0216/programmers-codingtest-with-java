e1ea6057d248541dee44754f95482d47a915d9d7
kwonjiyun <ziyun1612@ewhain.net>
2024-02-23 23:54:22 +0900

    정렬 : 요격시스템

---
+++ src/정렬/요격시스템.java
@@ -0,0 +1,29 @@
+package src.정렬;
+
+import java.util.Arrays;
+
+class 요격시스템
+{
+    public int solution(int[][] targets) {
+        int answer = 0;
+
+        Arrays.sort(targets, (o1, o2) -> {
+            if(o1[1] == o2[1]){
+                return o1[0] - o2[0];
+            }
+            return o1[1] - o2[1];
+        });
+
+        int end = targets[0][1];
+        answer++;
+
+        for(int[] tar : targets){
+            if(tar[0] >= end){
+                end = tar[1];
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
